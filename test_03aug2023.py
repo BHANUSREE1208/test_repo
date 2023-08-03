c30264287c7e74bae9bf88c1cd83b6b0f83eaf7e
BHANUSREE1208 <138585783+BHANUSREE1208@users.noreply.github.com>
2023-08-03 23:26:13 +0530

    Update test_03aug2023.py

--- test_03aug2023.py
+++ test_03aug2023.py
@@ -1,1 +1,9 @@
-print("hello world!")
+def find_pairs_with_sum(lst, N):
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[i] + lst[j] == N:
+                print("(", lst[i], ",", lst[j], ")")
+                
+l = [4, 3, 6, 8, 2, 9, 7, 5]
+N = 10
+find_pairs_with_sum(l, N)
